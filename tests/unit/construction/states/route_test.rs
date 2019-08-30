688e578814b4aa221f509e0a6bd52f8f0d0ddaf7
reinterpretcat <ilya.builuk@gmail.com>
2019-08-30 21:24:56 +0200

    Add more tests to route state

--- tests/unit/construction/states/route_test.rs
+++ tests/unit/construction/states/route_test.rs
@@ -19,6 +19,18 @@ fn can_put_and_get_activity_state() {
 }
 
 #[test]
+fn can_put_and_get_empty_activity_state() {
+    let mut route_state = RouteState::new((1, 1));
+    let activity1 = new_activity_ref();
+    let activity2 = new_activity_ref();
+
+    route_state.put_activity_state(1, &activity1, "my_value".to_string());
+    let result = route_state.get_activity_state::<String>(1, &activity2);
+
+    assert!(result.is_none());
+}
+
+#[test]
 fn can_put_and_get_activity_state_with_different_keys() {
     let mut route_state = RouteState::new((1, 1));
     let activity = new_activity_ref();
@@ -38,13 +50,36 @@ fn can_put_and_get_activity_state_with_different_keys() {
 }
 
 #[test]
-fn can_put_and_get_empty_state() {
+fn can_put_and_get_route_state() {
     let mut route_state = RouteState::new((1, 1));
-    let activity1 = new_activity_ref();
-    let activity2 = new_activity_ref();
 
-    route_state.put_activity_state(1, &activity1, "my_value".to_string());
-    let result = route_state.get_activity_state::<String>(1, &activity2);
+    route_state.put_route_state(1, "my_value".to_string());
+    let result = route_state.get_route_state::<String>(1);
+
+    assert_eq!(result.unwrap(), "my_value");
+}
+
+#[test]
+fn can_put_and_get_empty_route_state() {
+    let mut route_state = RouteState::new((1, 1));
+
+    route_state.put_route_state(1, "my_value".to_string());
+    let result = route_state.get_route_state::<String>(2);
 
     assert!(result.is_none());
+}
+
+#[test]
+fn can_remove_activity_states() {
+    let mut route_state = RouteState::new((1, 1));
+    let activity = new_activity_ref();
+
+    route_state.put_activity_state(1, &activity, "key1".to_string());
+    route_state.put_activity_state(2, &activity, "key2".to_string());
+    route_state.remove_activity_states(&activity);
+    let result1 = route_state.get_activity_state::<String>(1, &activity);
+    let result2 = route_state.get_activity_state::<String>(2, &activity);
+
+    assert!(result1.is_none());
+    assert!(result2.is_none());
 }
