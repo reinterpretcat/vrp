2db7bd9009bd6697587ca561a1d984debb994c3e
reinterpretcat <ilya.builuk@gmail.com>
2019-09-01 00:13:16 +0200

    Add test to registry

--- tests/unit/models/solution/actor_test.rs
+++ tests/unit/models/solution/actor_test.rs
@@ -2,6 +2,7 @@ use crate::helpers::models::problem::{test_driver, test_vehicle_detail, VehicleB
 use crate::models::common::TimeWindow;
 use crate::models::problem::{Fleet, VehicleDetail};
 use crate::models::solution::{Actor, Registry};
+use std::cmp::Ordering::Less;
 use std::sync::Arc;
 
 parameterized_test! {can_provide_available_actors_from_registry, (count, expected), {
@@ -44,4 +45,42 @@ fn can_provide_available_actors_from_registry_impl(count: usize, expected: usize
     let actors: Vec<Arc<Actor>> = registry.available().take(count).collect();
     actors.iter().for_each(|a| registry.use_actor(a));
     assert_eq!(registry.available().count(), expected);
+}
+
+#[test]
+fn can_provide_next_actors_from_registry() {
+    let fleet = Fleet::new(
+        vec![test_driver()],
+        vec![
+            VehicleBuilder::new()
+                .id("v1")
+                .details(vec![test_vehicle_detail()])
+                .build(),
+            VehicleBuilder::new()
+                .id("v2")
+                .details(vec![
+                    test_vehicle_detail(),
+                    VehicleDetail {
+                        start: Some(1),
+                        end: Some(0),
+                        time: Some(TimeWindow {
+                            start: 0.0,
+                            end: 50.0,
+                        }),
+                    },
+                ])
+                .build(),
+            VehicleBuilder::new()
+                .id("v3")
+                .details(vec![test_vehicle_detail()])
+                .build(),
+        ],
+    );
+    let mut registry = Registry::new(&fleet);
+
+    let mut actors: Vec<Arc<Actor>> = registry.next().collect();
+    actors.sort_by(|a, b| a.detail.start.partial_cmp(&b.detail.start).unwrap_or(Less));
+    assert_eq!(actors.len(), 2);
+    assert_eq!(actors.first().unwrap().detail.start, Some(0));
+    assert_eq!(actors.last().unwrap().detail.start, Some(1));
 }
