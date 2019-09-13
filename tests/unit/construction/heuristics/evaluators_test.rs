a7fae3698190145270a2c0d2d3ff1e5702d3b100
reinterpretcat <ilya.builuk@gmail.com>
2019-09-13 22:22:27 +0200

    Add test to insertion evaluator

--- tests/unit/construction/heuristics/evaluators_test.rs
+++ tests/unit/construction/heuristics/evaluators_test.rs
@@ -6,10 +6,13 @@ use crate::helpers::construction::states::test_insertion_progress;
 use crate::helpers::models::domain::{create_empty_problem, create_empty_problem_with_constraint};
 use crate::helpers::models::problem::*;
 use crate::helpers::models::solution::ActivityBuilder;
-use crate::models::common::{Location, Schedule, TimeWindow};
-use crate::models::problem::{Fleet, Job, Single};
+use crate::models::common::{Cost, Location, Schedule, TimeWindow};
+use crate::models::problem::{Fleet, Job, Single, VehicleDetail};
 use crate::models::solution::{Place, Registry};
+use crate::utils::compare_floats;
+use std::cmp::Ordering;
 use std::collections::HashSet;
+use std::ops::Deref;
 use std::sync::Arc;
 
 type JobPlace = crate::models::problem::Place;
@@ -140,5 +143,61 @@ fn can_insert_service_with_location_into_tour_with_two_activities_and_variations
     }
 }
 
-#[test]
-fn can_insert_service_with_location_into_tour_with_two_activities_and_locations_variations_impl() {}
+parameterized_test! {can_insert_service_with_two_vehicles_and_various_time_constraints, (job_location, v1_end_location, v2_end_location, expected_used_vehicle, cost), {
+    can_insert_service_with_two_vehicles_and_various_time_constraints_impl(job_location, v1_end_location, v2_end_location, expected_used_vehicle, cost);
+}}
+
+can_insert_service_with_two_vehicles_and_various_time_constraints! {
+    case1: (3, Some(0), Some(20), "v1", (3.0 + 3.0) * 2.0),
+    case2: (27, Some(0), Some(20), "v2", (7.0 + 7.0) * 2.0),
+    case3: (11, Some(12), Some(20), "v1", (12.0 + 12.0)),
+}
+
+fn can_insert_service_with_two_vehicles_and_various_time_constraints_impl(
+    job_location: Location,
+    v1_end_location: Option<Location>,
+    v2_end_location: Option<Location>,
+    expected_used_vehicle: &str,
+    cost: Cost,
+) {
+    let registry = Registry::new(&Fleet::new(
+        vec![test_driver_with_costs(empty_costs())],
+        vec![
+            VehicleBuilder::new()
+                .id("v1")
+                .details(vec![VehicleDetail {
+                    start: Some(0),
+                    end: v1_end_location,
+                    time: Some(TimeWindow { start: 0.0, end: 100.0 }),
+                }])
+                .build(),
+            VehicleBuilder::new()
+                .id("v2")
+                .details(vec![VehicleDetail {
+                    start: Some(20),
+                    end: v2_end_location,
+                    time: Some(TimeWindow { start: 0.0, end: 100.0 }),
+                }])
+                .build(),
+        ],
+    ));
+    let job = Arc::new(test_single_job_with_location(Some(job_location)));
+    let route_ctx = RouteContext::new(registry.next().next().unwrap());
+    let mut routes: HashSet<RouteContext> = HashSet::new();
+    routes.insert(route_ctx);
+    let mut constraint = create_constraint_pipeline_with_timing();
+    let ctx = create_insertion_context(registry, constraint, routes);
+
+    let result = InsertionEvaluator::new().evaluate(&job, &ctx);
+
+    if let InsertionResult::Success(success) = result {
+        assert_eq!(success.activities.len(), 1);
+        assert_eq!(
+            get_vehicle_id(success.context.route.read().unwrap().actor.vehicle.deref()),
+            &expected_used_vehicle.to_owned()
+        );
+        assert_eq!(compare_floats(&success.cost, &cost), Ordering::Equal);
+    } else {
+        assert!(false);
+    }
+}
