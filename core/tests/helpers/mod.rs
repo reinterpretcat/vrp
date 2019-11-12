7337b826bb26aa25987aa62625254f75769b01da
reinterpretcat <ilya.builuk@gmail.com>
2019-11-12 21:23:22 +0100

    Fix compiler warning

--- core/tests/helpers/mod.rs
+++ core/tests/helpers/mod.rs
@@ -4,5 +4,16 @@ pub mod objectives;
 pub mod refinement;
 pub mod utils;
 
+// See https://stackoverflow.com/questions/30429801/whats-the-most-idiomatic-way-to-test-two-options-for-equality-when-they-contain
+macro_rules! cmp_eq_option {
+    ($left:expr, $right:expr) => {{
+        match (&$left, &$right) {
+            (Some(left_val), Some(right_val)) => *left_val == *right_val,
+            (None, None) => true,
+            _ => false,
+        }
+    }};
+}
+
 #[macro_use]
 pub mod macros;
