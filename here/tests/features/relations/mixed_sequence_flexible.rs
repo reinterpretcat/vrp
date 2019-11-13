fd74c9a928ee7fcf3cd016fe3fed81326d3bf981
reinterpretcat <ilya.builuk@gmail.com>
2019-11-13 22:05:03 +0100

    Add test for here relations

--- here/tests/features/relations/mixed_sequence_flexible.rs
+++ here/tests/features/relations/mixed_sequence_flexible.rs
@@ -149,3 +149,185 @@ fn can_use_sequence_and_flexible_relation_for_one_vehicle() {
         }
     );
 }
+
+#[test]
+fn can_use_sequence_and_flexible_relation_for_two_vehicles() {
+    let problem = Problem {
+        id: "my_problem".to_string(),
+        plan: Plan {
+            jobs: vec![
+                create_delivery_job("job1", vec![1., 0.]),
+                create_delivery_job("job2", vec![2., 0.]),
+                create_delivery_job("job3", vec![3., 0.]),
+                create_delivery_job("job4", vec![4., 0.]),
+                create_delivery_job("job5", vec![5., 0.]),
+                create_delivery_job("job6", vec![6., 0.]),
+                create_delivery_job("job7", vec![7., 0.]),
+                create_delivery_job("job8", vec![8., 0.]),
+            ],
+            relations: Some(vec![
+                Relation {
+                    type_field: RelationType::Sequence,
+                    jobs: to_strings(vec!["departure", "job1", "job6"]),
+                    vehicle_id: "my_vehicle_1".to_string(),
+                },
+                Relation {
+                    type_field: RelationType::Flexible,
+                    jobs: to_strings(vec!["job3", "job7"]),
+                    vehicle_id: "my_vehicle_1".to_string(),
+                },
+                Relation {
+                    type_field: RelationType::Sequence,
+                    jobs: to_strings(vec!["departure", "job2", "job8"]),
+                    vehicle_id: "my_vehicle_2".to_string(),
+                },
+                Relation {
+                    type_field: RelationType::Flexible,
+                    jobs: to_strings(vec!["job4", "job5"]),
+                    vehicle_id: "my_vehicle_2".to_string(),
+                },
+            ]),
+        },
+        fleet: Fleet {
+            types: vec![VehicleType {
+                id: "my_vehicle".to_string(),
+                profile: "car".to_string(),
+                costs: create_default_vehicle_costs(),
+                places: create_default_open_vehicle_places(),
+                capacity: vec![5],
+                amount: 2,
+                skills: None,
+                limits: None,
+                vehicle_break: None,
+            }],
+        },
+    };
+    let matrix = Matrix {
+        num_origins: 9,
+        num_destinations: 9,
+        travel_times: vec![
+            0, 1, 2, 3, 4, 5, 6, 7, 1, 1, 0, 1, 2, 3, 4, 5, 6, 2, 2, 1, 0, 1, 2, 3, 4, 5, 3, 3, 2, 1, 0, 1, 2, 3, 4, 4,
+            4, 3, 2, 1, 0, 1, 2, 3, 5, 5, 4, 3, 2, 1, 0, 1, 2, 6, 6, 5, 4, 3, 2, 1, 0, 1, 7, 7, 6, 5, 4, 3, 2, 1, 0, 8,
+            1, 2, 3, 4, 5, 6, 7, 8, 0,
+        ],
+        distances: vec![
+            0, 1, 2, 3, 4, 5, 6, 7, 1, 1, 0, 1, 2, 3, 4, 5, 6, 2, 2, 1, 0, 1, 2, 3, 4, 5, 3, 3, 2, 1, 0, 1, 2, 3, 4, 4,
+            4, 3, 2, 1, 0, 1, 2, 3, 5, 5, 4, 3, 2, 1, 0, 1, 2, 6, 6, 5, 4, 3, 2, 1, 0, 1, 7, 7, 6, 5, 4, 3, 2, 1, 0, 8,
+            1, 2, 3, 4, 5, 6, 7, 8, 0,
+        ],
+        error_codes: Option::None,
+    };
+
+    let solution = solve_with_metaheuristic(problem, vec![matrix]);
+
+    assert_eq!(
+        solution,
+        Solution {
+            problem_id: "my_problem".to_string(),
+            statistic: Statistic {
+                cost: 80.,
+                distance: 26,
+                duration: 34,
+                times: Timing { driving: 26, serving: 8, waiting: 0, break_time: 0 },
+            },
+            tours: vec![
+                Tour {
+                    vehicle_id: "my_vehicle_1".to_string(),
+                    type_id: "my_vehicle".to_string(),
+                    stops: vec![
+                        create_stop_with_activity(
+                            "departure",
+                            "departure",
+                            (0., 0.),
+                            4,
+                            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:00Z"),
+                        ),
+                        create_stop_with_activity(
+                            "job1",
+                            "delivery",
+                            (1., 0.),
+                            3,
+                            ("1970-01-01T00:00:01Z", "1970-01-01T00:00:02Z"),
+                        ),
+                        create_stop_with_activity(
+                            "job6",
+                            "delivery",
+                            (6., 0.),
+                            2,
+                            ("1970-01-01T00:00:07Z", "1970-01-01T00:00:08Z"),
+                        ),
+                        create_stop_with_activity(
+                            "job3",
+                            "delivery",
+                            (3., 0.),
+                            1,
+                            ("1970-01-01T00:00:11Z", "1970-01-01T00:00:12Z"),
+                        ),
+                        create_stop_with_activity(
+                            "job7",
+                            "delivery",
+                            (7., 0.),
+                            0,
+                            ("1970-01-01T00:00:16Z", "1970-01-01T00:00:17Z"),
+                        )
+                    ],
+                    statistic: Statistic {
+                        cost: 40.,
+                        distance: 13,
+                        duration: 17,
+                        times: Timing { driving: 13, serving: 4, waiting: 0, break_time: 0 },
+                    },
+                },
+                Tour {
+                    vehicle_id: "my_vehicle_2".to_string(),
+                    type_id: "my_vehicle".to_string(),
+                    stops: vec![
+                        create_stop_with_activity(
+                            "departure",
+                            "departure",
+                            (0., 0.),
+                            4,
+                            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:00Z"),
+                        ),
+                        create_stop_with_activity(
+                            "job2",
+                            "delivery",
+                            (2., 0.),
+                            3,
+                            ("1970-01-01T00:00:02Z", "1970-01-01T00:00:03Z"),
+                        ),
+                        create_stop_with_activity(
+                            "job8",
+                            "delivery",
+                            (8., 0.),
+                            2,
+                            ("1970-01-01T00:00:09Z", "1970-01-01T00:00:10Z"),
+                        ),
+                        create_stop_with_activity(
+                            "job4",
+                            "delivery",
+                            (4., 0.),
+                            1,
+                            ("1970-01-01T00:00:14Z", "1970-01-01T00:00:15Z"),
+                        ),
+                        create_stop_with_activity(
+                            "job5",
+                            "delivery",
+                            (5., 0.),
+                            0,
+                            ("1970-01-01T00:00:16Z", "1970-01-01T00:00:17Z"),
+                        )
+                    ],
+                    statistic: Statistic {
+                        cost: 40.,
+                        distance: 13,
+                        duration: 17,
+                        times: Timing { driving: 13, serving: 4, waiting: 0, break_time: 0 },
+                    },
+                }
+            ],
+            unassigned: vec![],
+            extras: Extras { performance: vec![] },
+        }
+    );
+}
