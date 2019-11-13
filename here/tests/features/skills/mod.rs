c858ef716026468def00800196f2b7a26b89abc6
reinterpretcat <ilya.builuk@gmail.com>
2019-11-13 21:57:19 +0100

    Add skills test

--- here/tests/features/skills/mod.rs
+++ here/tests/features/skills/mod.rs
@@ -1,1 +1,2 @@
 mod basic_skill;
+mod unassigned_due_to_skills;
---
+++ here/tests/features/skills/unassigned_due_to_skills.rs
@@ -0,0 +1,37 @@
+use crate::helpers::*;
+use crate::json::problem::*;
+use crate::json::solution::*;
+
+#[test]
+fn can_have_unassigned_due_to_missing_vehicle_skill() {
+    let problem = Problem {
+        id: "my_problem".to_string(),
+        plan: Plan {
+            jobs: vec![create_delivery_job_with_skills("job1", vec![1., 0.], vec!["unique_skill".to_string()])],
+            relations: Option::None,
+        },
+        fleet: Fleet { types: vec![create_default_vehicle("vehicle_without_skill")] },
+    };
+    let matrix = create_matrix(vec![0, 1, 1, 0]);
+
+    let solution = solve_with_metaheuristic(problem, vec![matrix]);
+
+    assert_eq!(
+        solution,
+        Solution {
+            problem_id: "my_problem".to_string(),
+            statistic: Statistic {
+                cost: 0.,
+                distance: 0,
+                duration: 0,
+                times: Timing { driving: 0, serving: 0, waiting: 0, break_time: 0 },
+            },
+            tours: vec![],
+            unassigned: vec![UnassignedJob {
+                job_id: "job1".to_string(),
+                reasons: vec![UnassignedJobReason { code: 1, description: "cannot serve required skill".to_string() }]
+            }],
+            extras: Extras { performance: vec![] },
+        }
+    );
+}
