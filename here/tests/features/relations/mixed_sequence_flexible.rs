6be7b4a1964b20fad99f04b88e240cd803224d63
reinterpretcat <ilya.builuk@gmail.com>
2019-11-13 21:42:38 +0100

    Add test for here relations

---
+++ here/tests/features/relations/mixed_sequence_flexible.rs
@@ -0,0 +1,151 @@
+use crate::helpers::*;
+use crate::json::problem::*;
+use crate::json::solution::*;
+
+#[test]
+fn can_use_sequence_and_flexible_relation_for_one_vehicle() {
+    let problem = Problem {
+        id: "my_problem".to_string(),
+        plan: Plan {
+            jobs: vec![
+                create_delivery_job("job1", vec![1., 0.]),
+                create_delivery_job("job2", vec![2., 0.]),
+                create_delivery_job("job3", vec![3., 0.]),
+                create_delivery_job("job4", vec![4., 0.]),
+                create_delivery_job("job5", vec![5., 0.]),
+                create_delivery_job("job6", vec![6., 0.]),
+                create_delivery_job("job7", vec![7., 0.]),
+            ],
+            relations: Some(vec![
+                Relation {
+                    type_field: RelationType::Sequence,
+                    jobs: to_strings(vec!["departure", "job4", "job2", "job6"]),
+                    vehicle_id: "my_vehicle_1".to_string(),
+                },
+                Relation {
+                    type_field: RelationType::Flexible,
+                    jobs: to_strings(vec!["job1", "job3"]),
+                    vehicle_id: "my_vehicle_1".to_string(),
+                },
+            ]),
+        },
+        fleet: Fleet {
+            types: vec![VehicleType {
+                id: "my_vehicle".to_string(),
+                profile: "car".to_string(),
+                costs: create_default_vehicle_costs(),
+                places: create_default_vehicle_places(),
+                capacity: vec![10],
+                amount: 1,
+                skills: None,
+                limits: None,
+                vehicle_break: None,
+            }],
+        },
+    };
+    let matrix = Matrix {
+        num_origins: 8,
+        num_destinations: 8,
+        travel_times: vec![
+            0, 1, 2, 3, 4, 5, 6, 1, 1, 0, 1, 2, 3, 4, 5, 2, 2, 1, 0, 1, 2, 3, 4, 3, 3, 2, 1, 0, 1, 2, 3, 4, 4, 3, 2, 1,
+            0, 1, 2, 5, 5, 4, 3, 2, 1, 0, 1, 6, 6, 5, 4, 3, 2, 1, 0, 7, 1, 2, 3, 4, 5, 6, 7, 0,
+        ],
+        distances: vec![
+            0, 1, 2, 3, 4, 5, 6, 1, 1, 0, 1, 2, 3, 4, 5, 2, 2, 1, 0, 1, 2, 3, 4, 3, 3, 2, 1, 0, 1, 2, 3, 4, 4, 3, 2, 1,
+            0, 1, 2, 5, 5, 4, 3, 2, 1, 0, 1, 6, 6, 5, 4, 3, 2, 1, 0, 7, 1, 2, 3, 4, 5, 6, 7, 0,
+        ],
+        error_codes: Option::None,
+    };
+
+    let solution = solve_with_metaheuristic(problem, vec![matrix]);
+
+    assert_eq!(
+        solution,
+        Solution {
+            problem_id: "my_problem".to_string(),
+            statistic: Statistic {
+                cost: 61.,
+                distance: 22,
+                duration: 29,
+                times: Timing { driving: 22, serving: 7, waiting: 0, break_time: 0 },
+            },
+            tours: vec![Tour {
+                vehicle_id: "my_vehicle_1".to_string(),
+                type_id: "my_vehicle".to_string(),
+                stops: vec![
+                    create_stop_with_activity(
+                        "departure",
+                        "departure",
+                        (0., 0.),
+                        7,
+                        ("1970-01-01T00:00:00Z", "1970-01-01T00:00:00Z"),
+                    ),
+                    create_stop_with_activity(
+                        "job4",
+                        "delivery",
+                        (4., 0.),
+                        6,
+                        ("1970-01-01T00:00:04Z", "1970-01-01T00:00:05Z"),
+                    ),
+                    create_stop_with_activity(
+                        "job2",
+                        "delivery",
+                        (2., 0.),
+                        5,
+                        ("1970-01-01T00:00:07Z", "1970-01-01T00:00:08Z"),
+                    ),
+                    create_stop_with_activity(
+                        "job6",
+                        "delivery",
+                        (6., 0.),
+                        4,
+                        ("1970-01-01T00:00:12Z", "1970-01-01T00:00:13Z"),
+                    ),
+                    create_stop_with_activity(
+                        "job7",
+                        "delivery",
+                        (7., 0.),
+                        3,
+                        ("1970-01-01T00:00:14Z", "1970-01-01T00:00:15Z"),
+                    ),
+                    create_stop_with_activity(
+                        "job5",
+                        "delivery",
+                        (5., 0.),
+                        2,
+                        ("1970-01-01T00:00:17Z", "1970-01-01T00:00:18Z"),
+                    ),
+                    create_stop_with_activity(
+                        "job1",
+                        "delivery",
+                        (1., 0.),
+                        1,
+                        ("1970-01-01T00:00:22Z", "1970-01-01T00:00:23Z"),
+                    ),
+                    create_stop_with_activity(
+                        "job3",
+                        "delivery",
+                        (3., 0.),
+                        0,
+                        ("1970-01-01T00:00:25Z", "1970-01-01T00:00:26Z"),
+                    ),
+                    create_stop_with_activity(
+                        "arrival",
+                        "arrival",
+                        (0., 0.),
+                        0,
+                        ("1970-01-01T00:00:29Z", "1970-01-01T00:00:29Z"),
+                    )
+                ],
+                statistic: Statistic {
+                    cost: 61.,
+                    distance: 22,
+                    duration: 29,
+                    times: Timing { driving: 22, serving: 7, waiting: 0, break_time: 0 },
+                },
+            }],
+            unassigned: vec![],
+            extras: Extras { performance: vec![] },
+        }
+    );
+}
--- here/tests/features/relations/mod.rs
+++ here/tests/features/relations/mod.rs
@@ -1,3 +1,4 @@
+mod mixed_sequence_flexible;
 mod mixed_sequence_tour;
 mod strict_with_new_jobs;
 mod strict_with_old_jobs;
