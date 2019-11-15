fc2ebe1248348529020238835cc1a49f5e9ff644
reinterpretcat <ilya.builuk@gmail.com>
2019-11-15 21:07:52 +0100

    Consider vehicle cost differences as separate types

--- core/src/models/solution/registry.rs
+++ core/src/models/solution/registry.rs
@@ -2,14 +2,14 @@
 #[path = "../../../tests/unit/models/solution/actor_test.rs"]
 mod actor_test;
 
-use crate::models::problem::{Actor, ActorDetail, Fleet};
+use crate::models::problem::{Actor, ActorDetail, Costs, Fleet};
 use std::collections::{HashMap, HashSet};
 use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
 /// Specifies an entity responsible for providing actors and keeping track of their usage.
 pub struct Registry {
-    available: HashMap<ActorDetail, HashSet<Arc<Actor>>>,
+    available: HashMap<ActorKey, HashSet<Arc<Actor>>>,
     all: Vec<Arc<Actor>>,
 }
 
@@ -17,7 +17,7 @@ impl Registry {
     pub fn new(fleet: &Fleet) -> Registry {
         Registry {
             available: fleet.actors.iter().cloned().fold(HashMap::new(), |mut acc, actor| {
-                acc.entry(actor.detail.clone()).or_insert_with(HashSet::new).insert(actor.clone());
+                acc.entry(ActorKey::new(&actor)).or_insert_with(HashSet::new).insert(actor.clone());
                 acc
             }),
             all: fleet.actors.to_vec(),
@@ -26,12 +26,12 @@ impl Registry {
 
     /// Removes actor from the list of available actors.
     pub fn use_actor(&mut self, actor: &Arc<Actor>) {
-        self.available.get_mut(&actor.detail).unwrap().remove(actor);
+        self.available.get_mut(&ActorKey::new(&actor)).unwrap().remove(actor);
     }
 
     /// Adds actor to the list of available actors.
     pub fn free_actor(&mut self, actor: &Arc<Actor>) {
-        self.available.get_mut(&actor.detail).unwrap().insert(actor.clone());
+        self.available.get_mut(&ActorKey::new(&actor)).unwrap().insert(actor.clone());
     }
 
     /// Returns all actors.
@@ -55,24 +55,67 @@ impl Registry {
     }
 }
 
-impl PartialEq for ActorDetail {
-    fn eq(&self, other: &Self) -> bool {
-        other.start == self.start
-            && other.end == self.end
-            && other.time.start == self.time.start
-            && other.time.end == self.time.end
+#[derive(Clone)]
+struct ActorKey {
+    detail: ActorDetail,
+    driver_costs: Costs,
+    vehicle_costs: Costs,
+}
+
+impl ActorKey {
+    pub fn new(actor: &Actor) -> Self {
+        Self {
+            detail: actor.detail.clone(),
+            driver_costs: actor.driver.costs.clone(),
+            vehicle_costs: actor.vehicle.costs.clone(),
+        }
+    }
+
+    pub fn same_costs(&self, other: &Self) -> bool {
+        Self::compare_costs(&self.vehicle_costs, &other.vehicle_costs)
+            && Self::compare_costs(&self.driver_costs, &other.driver_costs)
+    }
+
+    fn compare_costs(lhs: &Costs, rhs: &Costs) -> bool {
+        lhs.fixed == rhs.fixed
+            && lhs.per_distance == rhs.per_distance
+            && lhs.per_driving_time == rhs.per_driving_time
+            && lhs.per_service_time == rhs.per_service_time
+            && lhs.per_waiting_time == rhs.per_waiting_time
     }
 }
 
-impl Eq for ActorDetail {}
+impl PartialEq for ActorKey {
+    fn eq(&self, other: &Self) -> bool {
+        other.same_costs(&self)
+            && other.detail.start == self.detail.start
+            && other.detail.end == self.detail.end
+            && other.detail.time.start == self.detail.time.start
+            && other.detail.time.end == self.detail.time.end
+    }
+}
 
-impl Hash for ActorDetail {
+impl Eq for ActorKey {}
+
+impl Hash for ActorKey {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.start.hash(state);
-        self.end.hash(state);
+        self.detail.start.hash(state);
+        self.detail.end.hash(state);
 
-        ((self.time.start * 1024.0 * 1024.0).round() as i64).hash(state);
-        ((self.time.end * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.detail.time.start * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.detail.time.end * 1024.0 * 1024.0).round() as i64).hash(state);
+
+        ((self.vehicle_costs.fixed * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.vehicle_costs.per_distance * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.vehicle_costs.per_driving_time * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.vehicle_costs.per_service_time * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.vehicle_costs.per_waiting_time * 1024.0 * 1024.0).round() as i64).hash(state);
+
+        ((self.driver_costs.fixed * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.driver_costs.per_distance * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.driver_costs.per_driving_time * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.driver_costs.per_service_time * 1024.0 * 1024.0).round() as i64).hash(state);
+        ((self.driver_costs.per_waiting_time * 1024.0 * 1024.0).round() as i64).hash(state);
     }
 }
 
