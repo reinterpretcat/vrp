36f8a638529f59080532629cb9396c4cad98954b
reinterpretcat <ilya.builuk@gmail.com>
2019-11-15 22:49:48 +0100

    Add test to check here permutation logic

--- here/tests/features/multjob/basic_multi_job.rs
+++ here/tests/features/multjob/basic_multi_job.rs
@@ -3,7 +3,7 @@ use crate::json::problem::*;
 use crate::json::solution::*;
 
 #[test]
-fn can_assign_properly_multi_and_single_job() {
+fn can_assign_multi_and_single_job_as_pickups_specified() {
     let problem = Problem {
         id: "my_problem".to_string(),
         plan: Plan {
@@ -88,6 +88,90 @@ fn can_assign_properly_multi_and_single_job() {
                     distance: 16,
                     duration: 20,
                     times: Timing { driving: 16, serving: 4, waiting: 0, break_time: 0 },
+                },
+            }],
+            unassigned: vec![],
+            extras: Extras { performance: vec![] },
+        }
+    );
+}
+
+#[test]
+fn can_assign_multi_job_in_pickup_effective_way() {
+    let problem = Problem {
+        id: "my_problem".to_string(),
+        plan: Plan {
+            jobs: vec![create_multi_job(
+                "multi",
+                vec![((4., 0.), 1., vec![1]), ((2., 0.), 1., vec![1])],
+                vec![((6., 0.), 1., vec![2])],
+            )],
+            relations: Option::None,
+        },
+        fleet: Fleet { types: vec![create_vehicle_with_capacity("my_vehicle", vec![2])] },
+    };
+    let matrix = create_matrix_from_problem(&problem);
+
+    let solution = solve_with_metaheuristic(problem, vec![matrix]);
+
+    assert_eq!(
+        solution,
+        Solution {
+            problem_id: "my_problem".to_string(),
+            statistic: Statistic {
+                cost: 37.,
+                distance: 12,
+                duration: 15,
+                times: Timing { driving: 12, serving: 3, waiting: 0, break_time: 0 },
+            },
+            tours: vec![Tour {
+                vehicle_id: "my_vehicle_1".to_string(),
+                type_id: "my_vehicle".to_string(),
+                stops: vec![
+                    create_stop_with_activity(
+                        "departure",
+                        "departure",
+                        (0., 0.),
+                        0,
+                        ("1970-01-01T00:00:00Z", "1970-01-01T00:00:00Z"),
+                    ),
+                    create_stop_with_activity_with_tag(
+                        "multi",
+                        "pickup",
+                        (2., 0.),
+                        1,
+                        ("1970-01-01T00:00:02Z", "1970-01-01T00:00:03Z"),
+                        "2"
+                    ),
+                    create_stop_with_activity_with_tag(
+                        "multi",
+                        "pickup",
+                        (4., 0.),
+                        2,
+                        ("1970-01-01T00:00:05Z", "1970-01-01T00:00:06Z"),
+                        "2"
+                    ),
+                    create_stop_with_activity_with_tag(
+                        "multi",
+                        "delivery",
+                        (6., 0.),
+                        0,
+                        ("1970-01-01T00:00:08Z", "1970-01-01T00:00:09Z"),
+                        "1"
+                    ),
+                    create_stop_with_activity(
+                        "arrival",
+                        "arrival",
+                        (0., 0.),
+                        0,
+                        ("1970-01-01T00:00:15Z", "1970-01-01T00:00:15Z"),
+                    )
+                ],
+                statistic: Statistic {
+                    cost: 37.,
+                    distance: 12,
+                    duration: 15,
+                    times: Timing { driving: 12, serving: 3, waiting: 0, break_time: 0 },
                 },
             }],
             unassigned: vec![],
