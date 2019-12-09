7a4bf4931685f289e414e86a7dfb0d20e7f30fa1
reinterpretcat <ilya.builuk@gmail.com>
2019-12-09 22:10:45 +0100

    Fix issue in multi tour processing logic

--- here/src/json/problem/job_reader.rs
+++ here/src/json/problem/job_reader.rs
@@ -289,7 +289,7 @@ fn read_reloads(
                 .collect::<Vec<_>>()
         })
         .for_each(|(reload_idx, job_id, mut single)| {
-            single.dimens.insert("tour_index".to_string(), Box::new(reload_idx));
+            single.dimens.insert("tour_index".to_string(), Box::new(reload_idx as usize));
             add_conditional_job(job_index, jobs, job_id, single);
         });
 }
