08a5289a4b5e3a2c10366e3be013f5cc241c4da5
reinterpretcat <ilya.builuk@gmail.com>
2019-12-11 23:23:59 +0100

    Reorganize reload tests

--- here/tests/features/reload/avoid_reload.rs
+++ here/tests/features/reload/avoid_reload.rs
@@ -3,18 +3,16 @@ use crate::json::problem::*;
 use crate::json::solution::*;
 
 #[test]
-fn can_serve_multi_job_and_delivery_with_reload() {
+fn can_serve_multi_job_and_delivery_in_one_tour_avoiding_reload() {
     let problem = Problem {
         id: "my_problem".to_string(),
         plan: Plan {
             jobs: vec![
-                create_delivery_job("simple1", vec![1., 0.]),
-                create_delivery_job("simple2", vec![3., 0.]),
-                create_delivery_job("simple3", vec![7., 0.]),
+                create_delivery_job("simple", vec![1., 0.]),
                 create_multi_job(
                     "multi",
                     vec![((2., 0.), 1., vec![1]), ((8., 0.), 1., vec![1])],
-                    vec![((9., 0.), 1., vec![2])],
+                    vec![((6., 0.), 1., vec![2])],
                 ),
             ],
             relations: Option::None,
@@ -26,7 +24,7 @@ fn can_serve_multi_job_and_delivery_with_reload() {
                 costs: create_default_vehicle_costs(),
                 shifts: vec![VehicleShift {
                     start: VehiclePlace { time: format_time(0), location: vec![0., 0.] },
-                    end: Some(VehiclePlace { time: format_time(100).to_string(), location: vec![10., 0.] }),
+                    end: Some(VehiclePlace { time: format_time(100).to_string(), location: vec![0., 0.] }),
                     breaks: None,
                     max_tours: Some(2),
                     load_time: Some(2),
@@ -49,9 +47,9 @@ fn can_serve_multi_job_and_delivery_with_reload() {
             problem_id: "my_problem".to_string(),
             statistic: Statistic {
                 cost: 46.,
-                distance: 14,
-                duration: 22,
-                times: Timing { driving: 14, serving: 8, waiting: 0, break_time: 0 },
+                distance: 16,
+                duration: 20,
+                times: Timing { driving: 16, serving: 4, waiting: 0, break_time: 0 },
             },
             tours: vec![Tour {
                 vehicle_id: "my_vehicle_1".to_string(),
@@ -65,70 +63,49 @@ fn can_serve_multi_job_and_delivery_with_reload() {
                         ("1970-01-01T00:00:00Z", "1970-01-01T00:00:00Z"),
                     ),
                     create_stop_with_activity(
-                        "simple1",
+                        "simple",
                         "delivery",
                         (1., 0.),
                         0,
                         ("1970-01-01T00:00:01Z", "1970-01-01T00:00:02Z"),
                     ),
-                    create_stop_with_activity(
-                        "reload",
-                        "reload",
-                        (0., 0.),
-                        2,
-                        ("1970-01-01T00:00:03Z", "1970-01-01T00:00:05Z")
-                    ),
-                    create_stop_with_activity(
-                        "simple2",
-                        "delivery",
-                        (3., 0.),
-                        1,
-                        ("1970-01-01T00:00:08Z", "1970-01-01T00:00:09Z"),
-                    ),
                     create_stop_with_activity_with_tag(
                         "multi",
                         "pickup",
                         (2., 0.),
-                        2,
-                        ("1970-01-01T00:00:10Z", "1970-01-01T00:00:11Z"),
-                        "1"
-                    ),
-                    create_stop_with_activity(
-                        "simple3",
-                        "delivery",
-                        (7., 0.),
                         1,
-                        ("1970-01-01T00:00:16Z", "1970-01-01T00:00:17Z"),
+                        ("1970-01-01T00:00:03Z", "1970-01-01T00:00:04Z"),
+                        "1"
                     ),
                     create_stop_with_activity_with_tag(
                         "multi",
                         "pickup",
                         (8., 0.),
                         2,
-                        ("1970-01-01T00:00:18Z", "1970-01-01T00:00:19Z"),
+                        ("1970-01-01T00:00:10Z", "1970-01-01T00:00:11Z"),
                         "2"
                     ),
                     create_stop_with_activity_with_tag(
                         "multi",
                         "delivery",
-                        (9., 0.),
+                        (6., 0.),
                         0,
-                        ("1970-01-01T00:00:20Z", "1970-01-01T00:00:21Z"),
+                        ("1970-01-01T00:00:13Z", "1970-01-01T00:00:14Z"),
                         "1"
                     ),
                     create_stop_with_activity(
                         "arrival",
                         "arrival",
-                        (10., 0.),
+                        (0., 0.),
                         0,
-                        ("1970-01-01T00:00:22Z", "1970-01-01T00:00:22Z"),
+                        ("1970-01-01T00:00:20Z", "1970-01-01T00:00:20Z"),
                     )
                 ],
                 statistic: Statistic {
                     cost: 46.,
-                    distance: 14,
-                    duration: 22,
-                    times: Timing { driving: 14, serving: 8, waiting: 0, break_time: 0 },
+                    distance: 16,
+                    duration: 20,
+                    times: Timing { driving: 16, serving: 4, waiting: 0, break_time: 0 },
                 },
             }],
             unassigned: vec![],
--- here/tests/features/reload/mod.rs
+++ here/tests/features/reload/mod.rs
@@ -1,2 +1,3 @@
+mod avoid_reload;
 mod basic_reload;
 mod multi_job_reload;
