2e5ebf4d434297295909a5709eb9759b0b30a0d6
reinterpretcat <ilya.builuk@gmail.com>
2020-02-19 20:35:31 +0100

    Fix load checker

--- vrp-pragmatic/tests/checker/capacity.rs
+++ vrp-pragmatic/tests/checker/capacity.rs
@@ -82,7 +82,11 @@ pub fn check_vehicle_load(context: &CheckerContext) -> Result<(), String> {
                         Capacity::default(),
                         |acc, activity| {
                             let activity_type = context.get_activity_type(tour, to, activity)?;
-                            let (demand_type, demand) = get_demand(context, &activity, &activity_type)?;
+                            let (demand_type, demand) = if activity.activity_type == "arrival" {
+                                (DemandType::StaticDelivery, end_pickup)
+                            } else {
+                                get_demand(context, &activity, &activity_type)?
+                            };
 
                             Ok(match demand_type {
                                 DemandType::StaticDelivery | DemandType::DynamicDelivery => acc - demand,
