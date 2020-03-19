c8d3ac532efc95856c422267af4c6d0e6f3ae963
reinterpretcat <ilya.builuk@gmail.com>
2020-03-20 00:09:22 +0100

    Improve color properties in geo json output

--- vrp-pragmatic/src/json/solution/geo_serializer.rs
+++ vrp-pragmatic/src/json/solution/geo_serializer.rs
@@ -45,10 +45,10 @@ fn get_marker_symbol(stop: &Stop) -> String {
     .to_string()
 }
 
-fn get_stop_point(tour_idx: usize, stop_idx: usize, stop: &Stop) -> Feature {
+fn get_stop_point(tour_idx: usize, stop_idx: usize, stop: &Stop, color: &str) -> Feature {
     Feature {
         properties: slice_to_map(&[
-            ("marker-color", "#0000ff"),
+            ("marker-color", color),
             ("marker-size", "medium"),
             ("marker-symbol", get_marker_symbol(&stop).as_str()),
             ("tour_idx", tour_idx.to_string().as_str()),
@@ -59,9 +59,15 @@ fn get_stop_point(tour_idx: usize, stop_idx: usize, stop: &Stop) -> Feature {
     }
 }
 
-fn get_tour_line(tour_idx: usize, tour: &Tour) -> Feature {
+fn get_tour_line(tour_idx: usize, tour: &Tour, color: &str) -> Feature {
     Feature {
-        properties: slice_to_map(&[("tour_idx", tour_idx.to_string().as_str())]),
+        properties: slice_to_map(&[
+            ("vehicle_id", tour.vehicle_id.as_str()),
+            ("tour_idx", tour_idx.to_string().as_str()),
+            ("shift_idx", tour.shift_index.to_string().as_str()),
+            ("stroke-width", "4"),
+            ("stroke", color),
+        ]),
         geometry: Geometry::LineString {
             coordinates: tour.stops.iter().map(|stop| (stop.location.lng, stop.location.lat)).collect(),
         },
@@ -71,13 +77,80 @@ fn get_tour_line(tour_idx: usize, tour: &Tour) -> Feature {
 /// Serializes solution into geo json format.
 pub fn serialize_solution_as_geojson<W: Write>(writer: BufWriter<W>, solution: &Solution) -> Result<(), Error> {
     let stop_markers = solution.tours.iter().enumerate().flat_map(|(tour_idx, tour)| {
-        tour.stops.iter().enumerate().map(move |(stop_idx, stop)| get_stop_point(tour_idx, stop_idx, &stop))
+        tour.stops.iter().enumerate().map(move |(stop_idx, stop)| {
+            get_stop_point(tour_idx, stop_idx, &stop, get_color_inverse(tour_idx).as_str())
+        })
     });
 
-    let stop_lines = solution.tours.iter().enumerate().map(|(tour_idx, tour)| get_tour_line(tour_idx, tour));
+    let stop_lines = solution
+        .tours
+        .iter()
+        .enumerate()
+        .map(|(tour_idx, tour)| get_tour_line(tour_idx, tour, get_color(tour_idx).as_str()));
 
     serde_json::to_writer_pretty(
         writer,
         &FeatureCollection { features: stop_markers.into_iter().chain(stop_lines.into_iter()).collect() },
     )
+}
+
+fn get_color(idx: usize) -> String {
+    static COLOR_LIST: ColorList = get_color_list();
+
+    let idx = idx % COLOR_LIST.len();
+
+    COLOR_LIST.get(idx).as_ref().unwrap().1.to_owned()
+}
+
+fn get_color_inverse(idx: usize) -> String {
+    static COLOR_LIST: ColorList = get_color_list();
+
+    let idx = (COLOR_LIST.len() - idx) % COLOR_LIST.len();
+
+    COLOR_LIST.get(idx).as_ref().unwrap().1.to_owned()
+}
+
+type ColorList = &'static [(&'static str, &'static str); 37];
+
+/// Returns list of human distinguishable colors.
+const fn get_color_list() -> ColorList {
+    &[
+        ("aqua", "#00ffff"),
+        ("black", "#000000"),
+        ("brown", "#a52a2a"),
+        ("darkblue", "#00008b"),
+        ("darkcyan", "#008b8b"),
+        ("darkgrey", "#a9a9a9"),
+        ("darkgreen", "#006400"),
+        ("darkkhaki", "#bdb76b"),
+        ("darkmagenta", "#8b008b"),
+        ("darkolivegreen", "#556b2f"),
+        ("darkorange", "#ff8c00"),
+        ("darkorchid", "#9932cc"),
+        ("darkred", "#8b0000"),
+        ("darksalmon", "#e9967a"),
+        ("darkviolet", "#9400d3"),
+        ("fuchsia", "#ff00ff"),
+        ("gold", "#ffd700"),
+        ("green", "#008000"),
+        ("indigo", "#4b0082"),
+        ("khaki", "#f0e68c"),
+        ("lightblue", "#add8e6"),
+        ("lightcyan", "#e0ffff"),
+        ("lightgreen", "#90ee90"),
+        ("lightgrey", "#d3d3d3"),
+        ("lightpink", "#ffb6c1"),
+        ("lightyellow", "#ffffe0"),
+        ("lime", "#00ff00"),
+        ("magenta", "#ff00ff"),
+        ("maroon", "#800000"),
+        ("navy", "#000080"),
+        ("olive", "#808000"),
+        ("orange", "#ffa500"),
+        ("pink", "#ffc0cb"),
+        ("purple", "#800080"),
+        ("red", "#ff0000"),
+        ("silver", "#c0c0c0"),
+        ("yellow", "#ffff00"),
+    ]
 }
