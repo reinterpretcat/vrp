447612596c576b407a89bba7000472e461318305
reinterpretcat <ilya.builuk@gmail.com>
2020-07-28 20:33:48 +0200

    Apply minor refactoring

--- vrp-pragmatic/tests/helpers/solver.rs
+++ vrp-pragmatic/tests/helpers/solver.rs
@@ -5,6 +5,7 @@ use std::cmp::Ordering::Less;
 use std::sync::Arc;
 use vrp_core::construction::heuristics::InsertionContext;
 use vrp_core::models::Problem as CoreProblem;
+use vrp_core::models::Solution as CoreSolution;
 use vrp_core::solver::mutation::{Recreate, RecreateWithCheapest};
 use vrp_core::solver::RefinementContext;
 use vrp_core::solver::{Builder, DominancePopulation};
@@ -12,24 +13,16 @@ use vrp_core::utils::DefaultRandom;
 
 /// Runs solver with cheapest insertion heuristic.
 pub fn solve_with_cheapest_insertion(problem: Problem, matrices: Option<Vec<Matrix>>) -> Solution {
-    let problem_copy = problem.clone();
-    let matrices_copy = matrices.clone();
+    get_core_solution(problem, matrices, |problem: Arc<CoreProblem>| {
+        let random = Arc::new(DefaultRandom::default());
+        let population = Box::new(DominancePopulation::new(problem.clone(), 8));
+        let mut refinement_ctx = RefinementContext::new(problem.clone(), population, None);
 
-    let problem = get_core_problem(problem, matrices);
-    let random = Arc::new(DefaultRandom::default());
-    let population = Box::new(DominancePopulation::new(problem.clone(), 8));
-    let mut refinement_ctx = RefinementContext::new(problem.clone(), population, None);
-
-    let solution = RecreateWithCheapest::default()
-        .run(&mut refinement_ctx, InsertionContext::new(problem.clone(), random))
-        .solution
-        .to_solution(problem.extras.clone());
-
-    let solution = create_solution(problem.as_ref(), &solution, None);
-
-    assert_eq!(CheckerContext::new(problem_copy, matrices_copy, solution.clone()).check().err(), None);
-
-    sort_all_data(solution)
+        RecreateWithCheapest::default()
+            .run(&mut refinement_ctx, InsertionContext::new(problem.clone(), random))
+            .solution
+            .to_solution(problem.extras.clone())
+    })
 }
 
 /// Runs solver with default metaheuristic and default amount of generations.
@@ -43,25 +36,18 @@ pub fn solve_with_metaheuristic_and_iterations(
     matrices: Option<Vec<Matrix>>,
     generations: usize,
 ) -> Solution {
-    let problem_copy = problem.clone();
-    let matrices_copy = matrices.clone();
+    get_core_solution(problem, matrices, |problem: Arc<CoreProblem>| {
+        let (solution, _, _) = Builder::new(problem)
+            .with_initial_methods(vec![(Box::new(RecreateWithCheapest::default()), 1)])
+            .with_max_generations(Some(generations))
+            .with_initial_size(1)
+            .build()
+            .unwrap_or_else(|err| panic!("cannot build solver: {}", err))
+            .solve()
+            .unwrap_or_else(|err| panic!("cannot solve the problem: {}", err));
 
-    let problem = get_core_problem(problem, matrices);
-
-    let (solution, _, _) = Builder::new(problem.clone())
-        .with_initial_methods(vec![(Box::new(RecreateWithCheapest::default()), 1)])
-        .with_max_generations(Some(generations))
-        .with_initial_size(1)
-        .build()
-        .unwrap_or_else(|err| panic!("cannot build solver: {}", err))
-        .solve()
-        .unwrap_or_else(|err| panic!("cannot solve the problem: {}", err));
-
-    let solution = sort_all_data(create_solution(problem.as_ref(), &solution, None));
-
-    assert_eq!(CheckerContext::new(problem_copy, matrices_copy, solution.clone()).check().err(), None);
-
-    solution
+        solution
+    })
 }
 
 fn get_core_problem(problem: Problem, matrices: Option<Vec<Matrix>>) -> Arc<CoreProblem> {
@@ -70,6 +56,24 @@ fn get_core_problem(problem: Problem, matrices: Option<Vec<Matrix>>) -> Arc<Core
             .ok()
             .unwrap(),
     )
+}
+
+fn get_core_solution<F: Fn(Arc<CoreProblem>) -> CoreSolution>(
+    problem: Problem,
+    matrices: Option<Vec<Matrix>>,
+    solve_func: F,
+) -> Solution {
+    let problem_copy = problem.clone();
+    let matrices_copy = matrices.clone();
+
+    let problem = get_core_problem(problem, matrices);
+
+    let solution = solve_func(problem.clone());
+
+    let solution = sort_all_data(create_solution(&problem, &solution, None));
+    assert_eq!(CheckerContext::new(problem_copy, matrices_copy, solution.clone()).check().err(), None);
+
+    sort_all_data(solution)
 }
 
 /// Sorts some solution properties in lexicographical order to simplify test assertions.
