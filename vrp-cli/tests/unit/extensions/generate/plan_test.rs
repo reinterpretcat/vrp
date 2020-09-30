7196042fc762c85e60405d72466d2ded16165848
reinterpretcat <ilya.builuk@gmail.com>
2020-09-30 18:22:01 +0200

    Update readme and add unit test

--- vrp-cli/tests/unit/extensions/generate/plan_test.rs
+++ vrp-cli/tests/unit/extensions/generate/plan_test.rs
@@ -15,3 +15,18 @@ fn can_generate_bounding_box() {
     assert_eq!(max_lat, 3.);
     assert_eq!(max_lng, 2.);
 }
+
+#[test]
+fn can_get_bounding_box_from_size() {
+    let plan = Plan {
+        jobs: vec![create_test_job(0., 1.), create_test_job(1., 0.), create_test_job(0., 0.), create_test_job(1., 1.)],
+        relations: None,
+    };
+
+    let ((min_lat, min_lng), (max_lat, max_lng)) = get_bounding_box_from_size(&plan, 100.);
+
+    assert!((min_lat - 0.499101).abs() < 1E-6);
+    assert!((min_lng - 0.499101).abs() < 1E-6);
+    assert!((max_lat - 0.500898).abs() < 1E-6);
+    assert!((max_lng - 0.500898).abs() < 1E-6);
+}
