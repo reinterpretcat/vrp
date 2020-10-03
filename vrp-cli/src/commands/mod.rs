4b015b6fb0b40a9fd66cf2cfcfb9f3f02410e183
reinterpretcat <ilya.builuk@gmail.com>
2020-10-04 01:13:40 +0200

    Apply minor refactoring

--- vrp-cli/src/commands/mod.rs
+++ vrp-cli/src/commands/mod.rs
@@ -1,10 +1,7 @@
 use clap::{App, Arg, ArgMatches, Values};
 
-#[cfg(not(target_arch = "wasm32"))]
 pub mod check;
-#[cfg(not(target_arch = "wasm32"))]
 pub mod generate;
-
 pub mod import;
 pub mod solve;
 
