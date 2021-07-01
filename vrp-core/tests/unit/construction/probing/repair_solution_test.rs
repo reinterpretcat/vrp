c2259aba4665d14e577fabb27953b8814b9f2515
reinterpretcat <ilya.builuk@gmail.com>
2021-07-01 22:09:28 +0200

    Commit work on tests for repair solution

--- vrp-core/tests/unit/construction/probing/repair_solution_test.rs
+++ vrp-core/tests/unit/construction/probing/repair_solution_test.rs
@@ -10,6 +10,7 @@ use crate::utils::Environment;
 type JobData = (Option<Location>, (f64, f64), Duration, i32);
 type VehicleData = ((Location, Option<f64>, Option<f64>), Option<(Location, Option<f64>, Option<f64>)>);
 type ActivityData = (Location, Duration, (Timestamp, Timestamp), Arc<Single>);
+type RouteData<'a> = Vec<(Location, Duration, (Timestamp, Timestamp), &'a str)>;
 
 fn create_test_problem(
     singles: Vec<(&str, JobData)>,
@@ -91,6 +92,22 @@ fn add_new_route(insertion_ctx: &mut InsertionContext, vehicle_id: &str, activit
     insertion_ctx.solution.routes.push(route_ctx);
 }
 
+fn add_routes(insertion_ctx: &mut InsertionContext, routes: Vec<(&str, RouteData)>) {
+    let problem = insertion_ctx.problem.clone();
+    routes.into_iter().for_each(|(vehicle_id, activities)| {
+        add_new_route(
+            insertion_ctx,
+            vehicle_id,
+            activities
+                .into_iter()
+                .map(|(location, duration, (s, e), job_id)| {
+                    (location, duration, (s, e), get_as_single(&problem, job_id))
+                })
+                .collect(),
+        );
+    });
+}
+
 fn get_job_by_id(problem: &Problem, job_id: &str) -> Job {
     problem.jobs.all().find(|job| job.dimens().get_id().unwrap() == job_id).clone().unwrap()
 }
@@ -99,31 +116,68 @@ fn get_as_single(problem: &Problem, job_id: &str) -> Arc<Single> {
     get_job_by_id(&problem, job_id).to_single().clone()
 }
 
-#[test]
-#[ignore]
-fn can_restore_solution() {
-    let problem = Arc::new(create_test_problem(
-        vec![
-            ("job1", (Some(1), (1., 3.), 1., 1)),
-            ("job2", (Some(2), (2., 4.), 1., 1)),
-            ("job3", (Some(3), (3., 5.), 1., 1)),
-        ],
-        vec![],
-        vec![("v1", ((0, Some(0.), None), Some((0, None, Some(10.)))))],
-    ));
-    let mut insertion_ctx = create_test_insertion_ctx(problem.clone());
-    add_new_route(
-        &mut insertion_ctx,
-        "v1",
-        vec![
-            (1, 1., (1., 3.), get_as_single(&problem, "job1")),
-            (2, 1., (2., 4.), get_as_single(&problem, "job2")),
-            (3, 1., (3., 5.), get_as_single(&problem, "job2")),
-        ],
-    );
-    problem.constraint.accept_solution_state(&mut insertion_ctx.solution);
+fn get_routes(insertion_ctx: &InsertionContext) -> Vec<(&str, Vec<&str>)> {
+    insertion_ctx
+        .solution
+        .routes
+        .iter()
+        .map(|route_ctx| {
+            (
+                route_ctx.route.actor.vehicle.dimens.get_id().unwrap().as_str(),
+                route_ctx
+                    .route
+                    .tour
+                    .all_activities()
+                    .flat_map(|a| a.job.as_ref())
+                    .map(|single| single.dimens.get_id().unwrap().as_str())
+                    .collect(),
+            )
+        })
+        .collect()
+}
 
-    let result = repair_solution_from_unknown(&insertion_ctx);
+mod single {
+    use super::*;
 
-    assert_eq!(result.solution.unassigned.len(), 0);
+    parameterized_test! {can_restore_solution_without_relations, (singles, vehicles, routes, expected), {
+        can_restore_solution_without_relations_impl(singles, vehicles, routes, expected);
+    }}
+
+    can_restore_solution_without_relations! {
+        case01: (vec![
+                    ("job1", (Some(1), (1., 3.), 1., 1)),
+                    ("job2", (Some(2), (2., 4.), 1., 1)),
+                    ("job3", (Some(3), (3., 5.), 1., 1)),
+                ],
+                vec![("v1", ((0, Some(0.), None), Some((0, None, Some(10.)))))],
+                vec![(
+                    "v1",
+                    vec![
+                        (1, 1., (1., 3.), "job1"),
+                        (2, 1., (2., 4.), "job2"),
+                        (3, 1., (3., 5.), "job3"),
+                    ]
+                )],
+                ((0, 0), vec![("v1", vec!["job1", "job2", "job3"])])
+        ),
+    }
+
+    fn can_restore_solution_without_relations_impl(
+        singles: Vec<(&str, JobData)>,
+        vehicles: Vec<(&str, VehicleData)>,
+        routes: Vec<(&str, RouteData)>,
+        expected: ((usize, usize), Vec<(&str, Vec<&str>)>),
+    ) {
+        let problem = Arc::new(create_test_problem(singles, vec![], vehicles));
+        let mut insertion_ctx = create_test_insertion_ctx(problem.clone());
+        add_routes(&mut insertion_ctx, routes);
+        problem.constraint.accept_solution_state(&mut insertion_ctx.solution);
+
+        let result = repair_solution_from_unknown(&insertion_ctx);
+
+        let ((unassigned, required), routes) = expected;
+        assert_eq!(result.solution.unassigned.len(), unassigned);
+        assert_eq!(result.solution.required.len(), required);
+        assert_eq!(get_routes(&result), routes);
+    }
 }
