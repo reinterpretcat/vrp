b357ebd13314e56aeccaba3113c3a0b0ce540fe8
reinterpretcat <ilya.builuk@gmail.com>
2021-11-05 21:23:34 +0100

    Commit progress on basic vicinity tests

--- vrp-pragmatic/tests/features/clustering/basic_vicinity_test.rs
+++ vrp-pragmatic/tests/features/clustering/basic_vicinity_test.rs
@@ -1,14 +1,12 @@
+use super::*;
 use crate::format::problem::*;
-use crate::format::solution::*;
-use crate::format_time;
-use crate::helpers::*;
 
 // TODO
-//  check different matrix/scale?
-//  check with constraints (e.g. skills)
 //  check two adjusted clusters
 //    - different stop location
 //    - same stop location (use limit by cluster size)
+//  check different matrix/scale?
+//  check with constraints (e.g. skills)
 
 fn create_test_problem(job_locations: &[f64], clustering: Clustering) -> Problem {
     Problem {
@@ -32,48 +30,48 @@ fn create_test_problem(job_locations: &[f64], clustering: Clustering) -> Problem
     }
 }
 
-type ExpectedActivity = ((f64, f64), Option<(f64, f64, f64)>, Option<(f64, f64, f64)>);
-
-parameterized_test! {can_cluster_simple_jobs, (visiting, serving, stop2_schedule, stop2_activity1, stop2_activity2, stop2_activity3, stop3_schedule, statistic), {
-    can_cluster_simple_jobs_impl(visiting, serving, stop2_schedule, stop2_activity1, stop2_activity2, stop2_activity3, stop3_schedule, statistic);
+parameterized_test! {can_cluster_simple_jobs, (visiting, serving, stop2, stop3_schedule, statistic), {
+    can_cluster_simple_jobs_impl(visiting, serving, StopData::new(stop2), stop3_schedule, statistic);
 }}
 
 can_cluster_simple_jobs! {
     case_01_continue: (
         VicinityVisitPolicy::Continue, VicinityServingPolicy::Original,
-        (3., 10.),
-        ((3., 4.), None, None),
-        ((5., 6.), Some((1., 4., 5.)), None),
-        ((7., 8.), Some((1., 6., 7.)), Some((2., 8., 10.))),
+        (3., 3, 1, (3., 10.), vec![
+          ActivityData::new(("job3", Some(3.), "delivery", Some((3., 4.)), Some((None, None)))),
+          ActivityData::new(("job2", Some(2.), "delivery", Some((5., 6.)), Some((Some((1., 4., 5.)), None)))),
+          ActivityData::new(("job1", Some(1.), "delivery", Some((7., 8.)), Some((Some((1., 6., 7.)), Some((2., 8., 10.)))))),
+        ]),
         (17., 18.),
-        (38., 10, 18, (10, 4, 4))
+        (38., 10, 18, (10, 4, 4)),
     ),
     case_02_return: (
-        VicinityVisitPolicy::Return, VicinityServingPolicy::Original,
-        (3., 12.),
-        ((3., 4.), None, None),
-        ((5., 6.), Some((1., 4., 5.)), Some((1., 6., 7.))),
-        ((9., 10.), Some((2., 7., 9.)), Some((2., 10., 12.))),
+       VicinityVisitPolicy::Return, VicinityServingPolicy::Original,
+        (3., 3, 1, (3., 12.), vec![
+          ActivityData::new(("job3", Some(3.), "delivery", Some((3., 4.)), Some((None, None)))),
+          ActivityData::new(("job2", Some(2.), "delivery", Some((5., 6.)), Some((Some((1., 4., 5.)), Some((1., 6., 7.)))))),
+          ActivityData::new(("job1", Some(1.), "delivery", Some((9., 10.)), Some((Some((2., 7., 9.)), Some((2., 10., 12.)))))),
+        ]),
         (19., 20.),
-        (40., 10, 20, (10, 4, 6))
+        (40., 10, 20, (10, 4, 6)),
     ),
-
     case_03_fixed: (
-        VicinityVisitPolicy::Continue, VicinityServingPolicy::Fixed { value: 5. },
-        (3., 22.),
-        ((3., 8.), None, None),
-        ((9., 14.), Some((1., 8., 9.)), None),
-        ((15., 20.), Some((1., 14., 15.)), Some((2., 20., 22.))),
+       VicinityVisitPolicy::Continue, VicinityServingPolicy::Fixed { value: 5. },
+        (3., 3, 1, (3., 22.), vec![
+          ActivityData::new(("job3", Some(3.), "delivery", Some((3., 8.)), Some((None, None)))),
+          ActivityData::new(("job2", Some(2.), "delivery", Some((9., 14.)), Some((Some((1., 8., 9.)), None)))),
+          ActivityData::new(("job1", Some(1.), "delivery", Some((15., 20.)), Some((Some((1., 14., 15.)), Some((2., 20., 22.)))))),
+        ]),
         (29., 30.),
         (50., 10, 30, (10, 16, 4))
     ),
-
     case_04_multiplier: (
-        VicinityVisitPolicy::Continue, VicinityServingPolicy::Multiplier { multiplier: 5. },
-        (3., 22.),
-        ((3., 8.), None, None),
-        ((9., 14.), Some((1., 8., 9.)), None),
-        ((15., 20.), Some((1., 14., 15.)), Some((2., 20., 22.))),
+       VicinityVisitPolicy::Continue, VicinityServingPolicy::Multiplier { multiplier: 5. },
+        (3., 3, 1, (3., 22.), vec![
+          ActivityData::new(("job3", Some(3.), "delivery", Some((3., 8.)), Some((None, None)))),
+          ActivityData::new(("job2", Some(2.), "delivery", Some((9., 14.)), Some((Some((1., 8., 9.)), None)))),
+          ActivityData::new(("job1", Some(1.), "delivery", Some((15., 20.)), Some((Some((1., 14., 15.)), Some((2., 20., 22.)))))),
+        ]),
         (29., 30.),
         (50., 10, 30, (10, 16, 4))
     ),
@@ -82,19 +80,10 @@ can_cluster_simple_jobs! {
 fn can_cluster_simple_jobs_impl(
     visiting: VicinityVisitPolicy,
     serving: VicinityServingPolicy,
-    stop2_schedule: (f64, f64),
-    stop2_activity1: ExpectedActivity,
-    stop2_activity2: ExpectedActivity,
-    stop2_activity3: ExpectedActivity,
+    stop2: StopData,
     stop3_schedule: (f64, f64),
     statistic: (f64, i64, i64, (i64, i64, i64)),
 ) {
-    let convert_expected_commute_info = |commute: Option<(f64, f64, f64)>| {
-        commute.map(|commute| CommuteInfo {
-            distance: commute.0,
-            time: Interval { start: format_time(commute.1), end: format_time(commute.2) },
-        })
-    };
     let statistic = Statistic {
         cost: statistic.0,
         distance: statistic.1,
@@ -143,59 +132,7 @@ fn can_cluster_simple_jobs_impl(
                         ("1970-01-01T00:00:00Z", "1970-01-01T00:00:00Z"),
                         0,
                     ),
-                    Stop {
-                        location: vec![3., 0.].to_loc(),
-                        time: Schedule {
-                            arrival: format_time(stop2_schedule.0),
-                            departure: format_time(stop2_schedule.1),
-                        },
-                        distance: 3,
-                        load: vec![1],
-                        activities: vec![
-                            Activity {
-                                job_id: "job3".to_string(),
-                                activity_type: "delivery".to_string(),
-                                location: Some(vec![3., 0.].to_loc()),
-                                time: Some(Interval {
-                                    start: format_time(stop2_activity1.0 .0),
-                                    end: format_time(stop2_activity1.0 .1),
-                                }),
-                                job_tag: None,
-                                commute: Some(Commute {
-                                    forward: convert_expected_commute_info(stop2_activity1.1),
-                                    backward: convert_expected_commute_info(stop2_activity1.2)
-                                }),
-                            },
-                            Activity {
-                                job_id: "job2".to_string(),
-                                activity_type: "delivery".to_string(),
-                                location: Some(vec![2., 0.].to_loc()),
-                                time: Some(Interval {
-                                    start: format_time(stop2_activity2.0 .0),
-                                    end: format_time(stop2_activity2.0 .1),
-                                }),
-                                job_tag: None,
-                                commute: Some(Commute {
-                                    forward: convert_expected_commute_info(stop2_activity2.1),
-                                    backward: convert_expected_commute_info(stop2_activity2.2)
-                                }),
-                            },
-                            Activity {
-                                job_id: "job1".to_string(),
-                                activity_type: "delivery".to_string(),
-                                location: Some(vec![1., 0.].to_loc()),
-                                time: Some(Interval {
-                                    start: format_time(stop2_activity3.0 .0),
-                                    end: format_time(stop2_activity3.0 .1),
-                                }),
-                                job_tag: None,
-                                commute: Some(Commute {
-                                    forward: convert_expected_commute_info(stop2_activity3.1),
-                                    backward: convert_expected_commute_info(stop2_activity3.2)
-                                }),
-                            },
-                        ],
-                    },
+                    stop2.into(),
                     create_stop_with_activity(
                         "job4",
                         "delivery",
--- vrp-pragmatic/tests/features/clustering/mod.rs
+++ vrp-pragmatic/tests/features/clustering/mod.rs
@@ -1,1 +1,74 @@
+use crate::format::solution::*;
+use crate::format::Location;
+use crate::format_time;
+use crate::helpers::*;
+use vrp_core::models::common::{Distance, Timestamp};
+
+type CommuteData = Option<(Distance, Timestamp, Timestamp)>;
+
+struct ActivityData {
+    job_id: String,
+    location: Option<f64>,
+    a_type: String,
+    time: Option<(Timestamp, Timestamp)>,
+    commute: Option<(CommuteData, CommuteData)>,
+}
+
+impl ActivityData {
+    pub fn new(
+        data: (&str, Option<f64>, &str, Option<(Timestamp, Timestamp)>, Option<(CommuteData, CommuteData)>),
+    ) -> Self {
+        Self { job_id: data.0.to_string(), location: data.1, a_type: data.2.to_string(), time: data.3, commute: data.4 }
+    }
+}
+
+impl From<ActivityData> for Activity {
+    fn from(activity: ActivityData) -> Self {
+        let convert_expected_commute_info = |commute: Option<(f64, f64, f64)>| {
+            commute.map(|commute| CommuteInfo {
+                distance: commute.0,
+                time: Interval { start: format_time(commute.1), end: format_time(commute.2) },
+            })
+        };
+
+        Activity {
+            job_id: activity.job_id,
+            activity_type: activity.a_type,
+            location: activity.location.map(|loc| vec![loc, 0.].to_loc()),
+            time: activity.time.map(|(start, end)| Interval { start: format_time(start), end: format_time(end) }),
+            job_tag: None,
+            commute: activity.commute.map(|(fwd, bak)| Commute {
+                forward: convert_expected_commute_info(fwd),
+                backward: convert_expected_commute_info(bak),
+            }),
+        }
+    }
+}
+
+struct StopData {
+    location: Location,
+    distance: i64,
+    load: i32,
+    time: (Timestamp, Timestamp),
+    activities: Vec<ActivityData>,
+}
+
+impl StopData {
+    pub fn new(data: (f64, i64, i32, (Timestamp, Timestamp), Vec<ActivityData>)) -> Self {
+        Self { location: vec![data.0, 0.].to_loc(), distance: data.1, load: data.2, time: data.3, activities: data.4 }
+    }
+}
+
+impl From<StopData> for Stop {
+    fn from(stop: StopData) -> Self {
+        Stop {
+            location: stop.location,
+            time: Schedule { arrival: format_time(stop.time.0), departure: format_time(stop.time.1) },
+            distance: stop.distance,
+            load: vec![stop.load],
+            activities: stop.activities.into_iter().map(ActivityData::into).collect(),
+        }
+    }
+}
+
 mod basic_vicinity_test;
