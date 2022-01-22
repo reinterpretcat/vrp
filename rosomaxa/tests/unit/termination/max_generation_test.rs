7b2d939efedb95b6201ddd7dde6882d7a29b7986
reinterpretcat <ilya.builuk@gmail.com>
2022-01-22 16:23:52 +0100

    Add test for max generations

--- rosomaxa/tests/unit/termination/max_generation_test.rs
+++ rosomaxa/tests/unit/termination/max_generation_test.rs
@@ -1,3 +1,21 @@
-#[test]
-#[ignore]
-fn can_use_max_generation() {}
+use super::*;
+use crate::helpers::example::create_default_heuristic_context;
+
+parameterized_test! {can_detect_termination, (generation, limit, expected), {
+    can_detect_termination_impl(generation, limit, expected);
+}}
+
+can_detect_termination! {
+    case_01: (11, 10, true),
+    case_02: (9, 10, false),
+    case_03: (10, 10, true),
+}
+
+fn can_detect_termination_impl(generation: usize, limit: usize, expected: bool) {
+    let mut context = create_default_heuristic_context();
+    context.statistics_mut().generation = generation;
+
+    let result = MaxGeneration::<_, _, _>::new(limit).is_termination(&mut context);
+
+    assert_eq!(result, expected);
+}
