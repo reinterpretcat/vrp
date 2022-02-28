80b8a71150d7713be482929fe5a5e6653774a035
reinterpretcat <ilya.builuk@gmail.com>
2022-02-28 22:20:54 +0100

    Refactor discovery test for breaks

--- vrp-pragmatic/tests/discovery/property/generated_with_breaks.rs
+++ vrp-pragmatic/tests/discovery/property/generated_with_breaks.rs
@@ -4,30 +4,79 @@ use crate::generator::*;
 use crate::helpers::solve_with_metaheuristic_and_iterations;
 use proptest::prelude::*;
 
-fn get_optional_breaks() -> impl Strategy<Value = Option<Vec<VehicleBreak>>> {
-    let places_proto = get_optional_break_places(get_optional_break_locations(), generate_durations(10..100));
-    let break_proto = generate_break(prop::collection::vec(places_proto, 1..2), get_optional_break_times(), Just(None));
+mod optional {
+    use super::*;
 
-    prop::collection::vec(break_proto, 1..2).prop_map(|break_| Some(break_))
-}
+    fn get_optional_breaks() -> impl Strategy<Value = Option<Vec<VehicleBreak>>> {
+        let places_proto = get_optional_break_places(
+            prop_oneof![Just(None), generate_location(&DEFAULT_BOUNDING_BOX).prop_map(|location| Some(location))],
+            generate_durations(10..100),
+        );
+        let break_proto = generate_break(
+            prop::collection::vec(places_proto, 1..2),
+            prop_oneof![get_optional_break_offset_time(), get_optional_break_time_windows()],
+            Just(None),
+        );
 
-fn get_optional_break_locations() -> impl Strategy<Value = Option<Location>> {
-    prop_oneof![Just(None), generate_location(&DEFAULT_BOUNDING_BOX).prop_map(|location| Some(location)),]
-}
+        prop::collection::vec(break_proto, 1..2).prop_map(|break_| Some(break_))
+    }
 
-prop_compose! {
-    pub fn get_optional_break_places(
-       locations: impl Strategy<Value = Option<Location>>,
-       durations: impl Strategy<Value = f64>,
-    )
-    (
-     location in locations,
-     duration in durations
-    ) -> VehicleOptionalBreakPlace {
-        VehicleOptionalBreakPlace {
-            location,
-            duration,
-            tag: None
+    prop_compose! {
+        pub fn get_optional_break_places(
+           locations: impl Strategy<Value = Option<Location>>,
+           durations: impl Strategy<Value = f64>,
+        )
+        (
+         location in locations,
+         duration in durations
+        ) -> VehicleOptionalBreakPlace {
+            VehicleOptionalBreakPlace { location, duration, tag: None }
+        }
+    }
+    prop_compose! {
+        fn get_optional_break_offset_time()
+        (
+         start in 3600..14400,
+         length in 600..1800
+        ) -> VehicleOptionalBreakTime {
+            VehicleOptionalBreakTime::TimeOffset(vec![start as f64, (start + length) as f64])
+        }
+    }
+
+    fn get_optional_break_time_windows() -> impl Strategy<Value = VehicleOptionalBreakTime> {
+        generate_multiple_time_windows_fixed(
+            START_DAY,
+            vec![from_hours(11), from_hours(13)],
+            vec![from_hours(2), from_hours(4)],
+            1..2,
+        )
+        .prop_map(|tws| VehicleOptionalBreakTime::TimeWindow(tws.first().unwrap().clone()))
+    }
+
+    prop_compose! {
+        fn get_vehicle_type_with_optional_breaks()
+        (
+         vehicle in default_vehicle_type_prototype(),
+         breaks in get_optional_breaks()
+        ) -> VehicleType {
+            assert_eq!(vehicle.shifts.len(), 1);
+
+            let mut vehicle = vehicle;
+            vehicle.shifts.first_mut().unwrap().breaks = breaks;
+
+            vehicle
+        }
+    }
+
+    prop_compose! {
+        pub(crate) fn get_problem_with_optional_breaks()
+        (
+         plan in generate_plan(generate_jobs(job_prototype(), 1..256)),
+         fleet in generate_fleet(
+            generate_vehicles(get_vehicle_type_with_optional_breaks(), 1..4),
+            default_matrix_profiles())
+        ) -> Problem {
+            Problem { plan, fleet, objectives: None }
         }
     }
 }
@@ -51,66 +100,11 @@ fn job_prototype() -> impl Strategy<Value = Job> {
     )
 }
 
-fn get_optional_break_times() -> impl Strategy<Value = VehicleOptionalBreakTime> {
-    prop_oneof![get_break_offset_time(), get_optional_break_time_windows()]
-}
-
-prop_compose! {
-    fn get_break_offset_time()
-    (
-     start in 3600..14400,
-     length in 600..1800
-    ) -> VehicleOptionalBreakTime {
-        VehicleOptionalBreakTime::TimeOffset(vec![start as f64, (start + length) as f64])
-    }
-}
-
-fn get_optional_break_time_windows() -> impl Strategy<Value = VehicleOptionalBreakTime> {
-    generate_multiple_time_windows_fixed(
-        START_DAY,
-        vec![from_hours(11), from_hours(13)],
-        vec![from_hours(2), from_hours(4)],
-        1..2,
-    )
-    .prop_map(|tws| VehicleOptionalBreakTime::TimeWindow(tws.first().unwrap().clone()))
-}
-
-prop_compose! {
-    fn get_vehicle_type_with_optional_breaks()
-    (
-     vehicle in default_vehicle_type_prototype(),
-     breaks in get_optional_breaks()
-    ) -> VehicleType {
-        assert_eq!(vehicle.shifts.len(), 1);
-
-        let mut vehicle = vehicle;
-        vehicle.shifts.first_mut().unwrap().breaks = breaks;
-
-        vehicle
-    }
-}
-
-prop_compose! {
-    fn get_problem_with_optional_breaks()
-    (
-     plan in generate_plan(generate_jobs(job_prototype(), 1..256)),
-     fleet in generate_fleet(
-        generate_vehicles(get_vehicle_type_with_optional_breaks(), 1..4),
-        default_matrix_profiles())
-    ) -> Problem {
-        Problem {
-            plan,
-            fleet,
-            objectives: None,
-        }
-    }
-}
-
 proptest! {
     #![proptest_config(ProptestConfig::with_cases(512))]
     #[test]
     #[ignore]
-    fn can_solve_problem_with_optional_breaks(problem in get_problem_with_optional_breaks()) {
+    fn can_solve_problem_with_optional_breaks(problem in optional::get_problem_with_optional_breaks()) {
         solve_with_metaheuristic_and_iterations(problem, None, 10);
     }
 }
