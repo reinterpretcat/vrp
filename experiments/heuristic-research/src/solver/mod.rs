92e23f4f48b971f347ea8a41b7670b4885a617be
reinterpretcat <ilya.builuk@gmail.com>
2022-04-09 12:29:02 +0200

    Print best solution in heuristic research

--- experiments/heuristic-research/src/solver/mod.rs
+++ experiments/heuristic-research/src/solver/mod.rs
@@ -2,6 +2,7 @@ use rosomaxa::evolution::TelemetryMode;
 use rosomaxa::example::*;
 use rosomaxa::get_default_population;
 use rosomaxa::prelude::*;
+use std::ops::Deref;
 use std::sync::Arc;
 
 mod objectives;
@@ -20,22 +21,33 @@ pub fn run_solver(
 ) {
     let fitness_fn = get_fitness_fn_by_name(function_name);
     let random = Arc::new(DefaultRandom::default());
-    let noise_op = VectorHeuristicOperatorMode::JustNoise(Noise::new(1., (-0.1, 0.1), random));
+    let noise_op = VectorHeuristicOperatorMode::JustNoise(Noise::new(1., (-0.1, 0.1), random.clone()));
 
-    let _ = Solver::default()
+    let (solutions, _) = Solver::default()
         .use_dynamic_heuristic_only()
         .with_logger(logger.clone())
         .with_init_solutions(vec![init_solution])
         .with_operator(noise_op, "first", 1.)
         .with_termination(None, Some(generations), None, None)
         .with_fitness_fn(fitness_fn)
-        .with_context_factory(Box::new(move |objective, environment| {
-            let inner =
-                get_default_population::<VectorContext, _, _>(objective.clone(), environment.clone(), selection_size);
-            let population = Box::new(ProxyPopulation::new(inner));
-            let telemetry_mode =
-                TelemetryMode::OnlyLogging { logger, log_best: 10, log_population: 100, dump_population: false };
-            VectorContext::new(objective, population, telemetry_mode, environment)
+        .with_context_factory(Box::new({
+            let logger = logger.clone();
+            move |objective, environment| {
+                let inner = get_default_population::<VectorContext, _, _>(
+                    objective.clone(),
+                    environment.clone(),
+                    selection_size,
+                );
+                let population = Box::new(ProxyPopulation::new(inner));
+                let telemetry_mode =
+                    TelemetryMode::OnlyLogging { logger, log_best: 20, log_population: 100, dump_population: false };
+                VectorContext::new(objective, population, telemetry_mode, environment)
+            }
         }))
-        .solve();
+        .solve()
+        .expect("no solutions");
+
+    let (individual, fitness) = solutions.first().expect("empty solutions");
+
+    logger.deref()(&format!("solution: {:?}, fitness: {}", individual, fitness));
 }
