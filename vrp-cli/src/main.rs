b9c9610d63f0a9b629f35f928f149275242f86ee
reinterpretcat <ilya.builuk@gmail.com>
2022-05-13 22:17:17 +0200

    Bump version to 1.18.1

--- vrp-cli/src/main.rs
+++ vrp-cli/src/main.rs
@@ -29,7 +29,7 @@ mod cli {
 
     pub fn get_app() -> Command<'static> {
         Command::new("Vehicle Routing Problem Solver")
-            .version("1.18.0")
+            .version("1.18.1")
             .author("Ilya Builuk <ilya.builuk@gmail.com>")
             .about("A command line interface to Vehicle Routing Problem solver")
             .subcommand(get_analyze_app())
