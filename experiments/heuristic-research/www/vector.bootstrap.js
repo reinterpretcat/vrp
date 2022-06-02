a58971a1ddb00a9dfcd61565aad8f47996604b3c
reinterpretcat <ilya.builuk@gmail.com>
2022-06-02 22:19:24 +0200

    Move vector experiment to separate files

--- experiments/heuristic-research/www/vector.bootstrap.js
+++ experiments/heuristic-research/www/vector.bootstrap.js
@@ -3,7 +3,7 @@ init();
 async function init() {
     const [{Chart, default: init, run_experiment, clear}, {main, setup}] = await Promise.all([
         import("../pkg/heuristic_research.js"),
-        import("./index.js"),
+        import("./vector.index.js"),
     ]);
     await init();
     setup(Chart, run_experiment, clear);
