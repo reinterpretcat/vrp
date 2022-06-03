942a2988edcf8a43b4f18ea54d70a5e6686398ce
reinterpretcat <ilya.builuk@gmail.com>
2022-06-03 23:41:10 +0200

    Add file selector

--- experiments/heuristic-research/www/vrp.index.js
+++ experiments/heuristic-research/www/vrp.index.js
@@ -1,9 +1,26 @@
+const fileBrowser = document.getElementById("file-browser");
+
 /** Main entry point */
 export function main() {
-    // TODO
+    setupUI();
 }
 
 /** This function is used in `vrp.bootstrap.js` to setup imports. */
 export function setup(run_vrp_experiment, clear) {
     // TODO
+}
+
+function setupUI() {
+    fileBrowser.addEventListener("change", openFile);
+}
+
+function openFile(event) {
+    let input = event.target;
+    let reader = new FileReader();
+
+    reader.onload = function(){
+        let content = reader.result;
+        console.log(content.substring(0, 300));
+    };
+    reader.readAsText(input.files[0]);
 }
