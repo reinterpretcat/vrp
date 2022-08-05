cf3ca857f7629f529503267048131ab3aaaaaf28
reinterpretcat <ilya.builuk@gmail.com>
2022-08-05 21:17:53 +0200

    Commit progress on shared resource test

--- vrp-core/tests/unit/construction/constraints/shared_resource_test.rs
+++ vrp-core/tests/unit/construction/constraints/shared_resource_test.rs
@@ -1,14 +1,15 @@
 use self::ActivityType::*;
 use super::*;
+use crate::construction::extensions::route_intervals;
 use crate::helpers::models::domain::create_empty_solution_context;
 use crate::helpers::models::problem::*;
 use crate::helpers::models::solution::{create_route_context_with_activities, test_activity};
 use crate::models::common::*;
 use crate::models::problem::Fleet;
 
-fn create_terminal_activity() -> Activity {
-    Activity { job: None, ..test_activity() }
-}
+const CODE: i32 = 1;
+const RESOURCE_KEY: i32 = 1;
+const INTERVALS_KEY: i32 = 2;
 
 fn create_usage_activity(demand: i32) -> Activity {
     let demand = SingleDimLoad::new(demand);
@@ -20,24 +21,22 @@ fn create_usage_activity(demand: i32) -> Activity {
     Activity { job: Some(single), ..test_activity() }
 }
 
-fn create_resource_activity(capacity: i32, resource_id: SharedResourceId) -> Activity {
+fn create_resource_activity(capacity: i32, resource_id: Option<SharedResourceId>) -> Activity {
     let mut single = test_single();
-    single.dimens.set_value("resource_id", resource_id);
+    if let Some(resource_id) = resource_id {
+        single.dimens.set_value("resource_id", resource_id);
+    }
     single.dimens.set_capacity(SingleDimLoad::new(capacity));
 
     Activity { job: Some(Arc::new(single)), ..test_activity() }
 }
 
 fn create_shared_resource_model(total_jobs: usize) -> SharedResourceModule<SingleDimLoad> {
-    let code = 1;
-    let resource_key = 1;
-    let intervals_key = 2;
-
     SharedResourceModule::new(
         total_jobs,
-        code,
-        resource_key,
-        Arc::new(move |route_ctx| route_ctx.state.get_route_state::<Vec<(usize, usize)>>(intervals_key)),
+        CODE,
+        RESOURCE_KEY,
+        Arc::new(move |route_ctx| route_ctx.state.get_route_state::<Vec<(usize, usize)>>(INTERVALS_KEY)),
         Arc::new(|activity| {
             activity.job.as_ref().and_then(|job| {
                 job.dimens.get_capacity().cloned().zip(job.dimens.get_value::<SharedResourceId>("resource_id").cloned())
@@ -56,36 +55,72 @@ fn create_route_ctx(
     let activities = activities
         .into_iter()
         .map(|activity_type| match activity_type {
-            Terminal => create_terminal_activity(),
-            Resource(resource_id) => {
-                create_resource_activity(resources.get(&resource_id).unwrap().clone(), resource_id)
+            SharedResource(resource_id) => {
+                create_resource_activity(resources.get(&resource_id).unwrap().clone(), Some(resource_id))
             }
+            NormalResource(capacity) => create_resource_activity(capacity, None),
             Usage(demand) => create_usage_activity(demand),
         })
         .collect();
-    create_route_context_with_activities(&fleet, vehicle_id, activities)
+
+    let mut route_ctx = create_route_context_with_activities(&fleet, vehicle_id, activities);
+    let intervals = route_intervals(&route_ctx.route, |activity| {
+        activity.job.as_ref().map_or(true, |job| {
+            let capacity: Option<&SingleDimLoad> = job.dimens.get_capacity();
+            capacity.is_some()
+        })
+    });
+    route_ctx.state_mut().put_route_state(INTERVALS_KEY, intervals);
+
+    route_ctx
 }
 
 enum ActivityType {
-    Terminal,
     Usage(i32),
-    Resource(usize),
+    NormalResource(i32),
+    SharedResource(usize),
 }
 
 #[test]
 fn can_update_resource_consumption() {
     let resources = [(0, 10), (1, 5)].into_iter().collect::<HashMap<usize, _>>();
-    let v1_activities = vec![Terminal, Usage(2), Resource(0), Usage(2)];
+    let v1_activities = vec![Usage(2), SharedResource(0), Usage(2)];
+    let v2_activities = vec![Usage(2), SharedResource(0), Usage(2)];
+    let expected_resources: Vec<Vec<Option<i32>>> = vec![];
 
+    let total_jobs = v1_activities.len() + v2_activities.len();
     let fleet = FleetBuilder::default()
         .add_driver(test_driver())
         .add_vehicle(test_vehicle_with_id("v1"))
         .add_vehicle(test_vehicle_with_id("v2"))
-        .add_vehicle(test_vehicle_with_id("v3"))
         .build();
-
-    let _solution_ctx = SolutionContext {
-        routes: vec![create_route_ctx(&fleet, "v1", &resources, v1_activities)],
+    let mut solution_ctx = SolutionContext {
+        routes: vec![
+            create_route_ctx(&fleet, "v1", &resources, v1_activities),
+            create_route_ctx(&fleet, "v2", &resources, v2_activities),
+        ],
         ..create_empty_solution_context()
     };
+    let shared_resource_module = create_shared_resource_model(total_jobs);
+
+    shared_resource_module.accept_solution_state(&mut solution_ctx);
+
+    let actual_resources = solution_ctx
+        .routes
+        .iter()
+        .map(|route_ctx| {
+            route_ctx
+                .route
+                .tour
+                .all_activities()
+                .map(|activity| {
+                    route_ctx
+                        .state
+                        .get_activity_state::<SingleDimLoad>(RESOURCE_KEY, activity)
+                        .map(|resource| resource.value)
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    assert_eq!(actual_resources, expected_resources);
 }
