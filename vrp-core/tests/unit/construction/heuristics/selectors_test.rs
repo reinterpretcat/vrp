edaf09dcc618e7b4d723a2563693ddf7ecc340c3
reinterpretcat <ilya.builuk@gmail.com>
2022-12-10 11:47:03 +0100

    Apply minor refactoring

--- vrp-core/tests/unit/construction/heuristics/selectors_test.rs
+++ vrp-core/tests/unit/construction/heuristics/selectors_test.rs
@@ -2,51 +2,56 @@ use super::*;
 use crate::helpers::models::domain::create_empty_insertion_context;
 use crate::helpers::models::problem::test_single_with_id;
 use crate::helpers::models::solution::create_empty_route_ctx;
+use crate::helpers::solver::generate_matrix_routes_with_defaults;
 use crate::helpers::utils::random::FakeRandom;
 use crate::models::common::Cost;
 use std::sync::Arc;
 
-fn make_success(cost: Cost) -> InsertionResult {
-    InsertionResult::make_success(cost, Job::Single(test_single_with_id("job1")), vec![], create_empty_route_ctx())
-}
+mod noise_checks {
+    use super::*;
 
-parameterized_test! {can_compare_insertion_result_with_noise, (left, right, reals, expected_result), {
-    can_compare_insertion_result_with_noise_impl(left, right, reals, expected_result);
-}}
+    fn make_success(cost: Cost) -> InsertionResult {
+        InsertionResult::make_success(cost, Job::Single(test_single_with_id("job1")), vec![], create_empty_route_ctx())
+    }
 
-can_compare_insertion_result_with_noise! {
-    case_01: (make_success(10.), make_success(11.), vec![0.05, 1.2, 0.05, 1.],  Some(11.)),
-    case_02: (make_success(11.), make_success(10.), vec![0.05, 0.8, 0.05, 1.],  Some(11.)),
-    case_03: (make_success(11.), make_success(10.), vec![0.05, 1., 0.2],  Some(10.)),
+    parameterized_test! {can_compare_insertion_result_with_noise, (left, right, reals, expected_result), {
+        can_compare_insertion_result_with_noise_impl(left, right, reals, expected_result);
+    }}
 
-    case_04: (InsertionResult::make_failure(), make_success(11.), vec![],  Some(11.)),
-    case_05: (make_success(10.), InsertionResult::make_failure(), vec![],  Some(10.)),
-    case_06: (InsertionResult::make_failure(), InsertionResult::make_failure(), vec![],  None),
-}
+    can_compare_insertion_result_with_noise! {
+        case_01: (make_success(10.), make_success(11.), vec![0.05, 1.2, 0.05, 1.],  Some(11.)),
+        case_02: (make_success(11.), make_success(10.), vec![0.05, 0.8, 0.05, 1.],  Some(11.)),
+        case_03: (make_success(11.), make_success(10.), vec![0.05, 1., 0.2],  Some(10.)),
 
-fn can_compare_insertion_result_with_noise_impl(
-    left: InsertionResult,
-    right: InsertionResult,
-    reals: Vec<f64>,
-    expected_result: Option<f64>,
-) {
-    let noise_probability = 0.1;
-    let noise_range = (0.9, 1.2);
-    let random = Arc::new(FakeRandom::new(vec![], reals));
-    let noise = Noise::new(noise_probability, noise_range, random);
+        case_04: (InsertionResult::make_failure(), make_success(11.), vec![],  Some(11.)),
+        case_05: (make_success(10.), InsertionResult::make_failure(), vec![],  Some(10.)),
+        case_06: (InsertionResult::make_failure(), InsertionResult::make_failure(), vec![],  None),
+    }
 
-    let actual_result =
-        NoiseResultSelector::new(noise).select_insertion(&create_empty_insertion_context(), left, right);
+    fn can_compare_insertion_result_with_noise_impl(
+        left: InsertionResult,
+        right: InsertionResult,
+        reals: Vec<f64>,
+        expected_result: Option<f64>,
+    ) {
+        let noise_probability = 0.1;
+        let noise_range = (0.9, 1.2);
+        let random = Arc::new(FakeRandom::new(vec![], reals));
+        let noise = Noise::new(noise_probability, noise_range, random);
 
-    match (actual_result, expected_result) {
-        (InsertionResult::Success(success), Some(cost)) => assert_eq!(success.cost, cost),
-        (InsertionResult::Failure(_), None) => {}
-        _ => unreachable!(),
+        let actual_result =
+            NoiseResultSelector::new(noise).select_insertion(&create_empty_insertion_context(), left, right);
+
+        match (actual_result, expected_result) {
+            (InsertionResult::Success(success), Some(cost)) => assert_eq!(success.cost, cost),
+            (InsertionResult::Failure(_), None) => {}
+            _ => unreachable!(),
+        }
     }
 }
 
 mod iterators {
-    use crate::helpers::solver::generate_matrix_routes_with_defaults;
+    use super::*;
 
     #[test]
     fn can_get_size_hint_for_tour_legs() {
@@ -57,13 +62,8 @@ mod iterators {
 }
 
 mod selections {
-    use super::LegSelection;
-    use crate::construction::heuristics::RouteContext;
+    use super::*;
     use crate::helpers::models::problem::SingleBuilder;
-    use crate::helpers::solver::generate_matrix_routes_with_defaults;
-    use crate::models::solution::Leg;
-    use rosomaxa::utils::Environment;
-    use std::sync::Arc;
 
     parameterized_test! {can_use_stochastic_selection_mode, (skip, activities, expected_threshold), {
         can_use_stochastic_selection_mode_impl(skip, activities, expected_threshold);
