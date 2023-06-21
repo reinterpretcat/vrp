0ba291ed7a2ebb6e5355c010ac7f32d82f9bd26f
reinterpretcat <ilya.builuk@gmail.com>
2023-06-21 20:45:01 +0200

    Research: tweak parameters

--- experiments/heuristic-research/src/plots/drawing/draw_heuristic.rs
+++ experiments/heuristic-research/src/plots/drawing/draw_heuristic.rs
@@ -24,13 +24,13 @@ pub fn draw_heuristic<B: DrawingBackend + 'static>(
     chart.configure_mesh().draw()?;
 
     chart.draw_series((0..).zip(data.iter()).map(|(y, x)| {
-        let mut bar = Rectangle::new([(0.0, SegmentValue::Exact(y)), (*x, SegmentValue::Exact(y + 1))], GREEN.filled());
-        bar.set_margin(5, 5, 0, 0);
+        let mut bar = Rectangle::new([(0.0, SegmentValue::Exact(y)), (*x, SegmentValue::Exact(y + 1))], RED.filled());
+        bar.set_margin(2, 2, 0, 0);
         bar
     }))?;
 
     chart.draw_series((0..).zip(labels.iter()).map(|(y, label)| {
-        Text::new(label.clone(), (0.0, SegmentValue::Exact(y + 1)), ("sans-serif", font_size).into_font().color(&RED))
+        Text::new(label.clone(), (0.0, SegmentValue::Exact(y + 1)), ("sans-serif", font_size).into_font().color(&BLACK))
     }))?;
 
     Ok(())
