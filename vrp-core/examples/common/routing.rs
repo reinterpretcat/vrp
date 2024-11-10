60a1f26eaf1ed820edf4823f766bf47e6fb21671
reinterpretcat <ilya.builuk@gmail.com>
2024-11-10 17:18:24 +0100

    Fix formatting

--- vrp-core/examples/common/routing.rs
+++ vrp-core/examples/common/routing.rs
@@ -7,8 +7,8 @@ pub fn define_routing_data() -> GenericResult<impl TransportCost> {
     // define distance/duration matrix (use the same data for both)
     // as we have five locations, we need to define 5x5 matrix, flatten to 1 dimension:
     #[rustfmt::skip]
-        let routing_data = vec![
-        //  0     1     2     3     4
+    let routing_data = vec![
+    //  0     1     2     3     4
         0.,  500., 520., 530., 540.,  // 0
         500.,  0.,  30.,  40.,  50.,  // 1
         520., 30.,   0.,  20.,  25.,  // 2
